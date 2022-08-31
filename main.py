6062243320d7a2fa79f73cd5ac2bd12378843156
prabesh412 <prabeshuprety1@gmail.com>
2022-08-31 21:18:49 +0545

    finally

--- main.py
+++ main.py
@@ -1,23 +1,49 @@
 import requests
-import pandas as pd
-from pandas import *
 import json
 
-url = "https://bg.annapurnapost.com/api/tags/news?page=1&per_page=40&tag=corona-virus"
 
-payload={}
+url = "https://bg.annapurnapost.com/api/search?page=1&title=%E0%A4%95%E0%A5%8B%E0%A4%B0%E0%A5%8B%E0%A4%A8%E0%A4%BE"
+payload = {}
 headers = {}
-res = []
-response = requests.request("GET", url, headers=headers, data=payload)
+response = requests.request("GET", url,timeout=10, headers=headers, data=payload)
 data = response.json()
-for i in data['data']:
-    res.append(i)
+last = data['data']['totalPage']
+with open("data.json","r",encoding='utf-8') as f:
+    try:
+        json_object = json.load(f)
+        for i in json_object:
+            next_page = i['page']
+    except:
+        next_page = 1
 
-df = pd.json_normalize(res)
-df.to_csv('result.csv')
+def request(page, last_page):
+    dictobj=[]
+    for r in range(page, last_page):
+        url = f"https://bg.annapurnapost.com/api/search?page={page}&title=%E0%A4%95%E0%A5%8B%E0%A4%B0%E0%A5%8B%E0%A4%A8%E0%A4%BE"
 
-data = read_csv("result.csv")
-slug = data['title'].tolist()
+        payload={}
+        headers = {}
+        res = []
+        response = requests.request("GET", url, headers=headers, data=payload)
 
-with open('data.json', 'w', encoding='utf-8') as f:
-    json.dump({"titles":slug}, f, ensure_ascii=False, indent=4)
+        data = response.json()
+
+        for i in data['data']['items']:
+
+            res.append(i["title"])
+
+        page += 1
+
+        dictobj.append({"page": page - 1,
+                        "title": res})
+        with open('data.json', 'r+', encoding='utf-8') as json_file:
+            try:
+                listobj = json.load(json_file)
+                listobj.append({"page": page - 1,"title": res})
+
+                with open('data.json', 'w', encoding='utf-8') as json_file:
+                    json.dump(listobj, json_file, ensure_ascii=False, indent=4, separators=(',', ': '))
+            except:
+                with open('data.json', 'w', encoding='utf-8') as json_file:
+                    json.dump(dictobj, json_file, ensure_ascii=False, indent=4, separators=(',', ': '))
+request(next_page,last)
