6b968bf1b4c620ae22fdab5fbe7993c72687dd77
prabesh412 <prabeshuprety1@gmail.com>
2022-08-31 15:49:49 +0545

    first commit

---
+++ main.py
@@ -0,0 +1,23 @@
+import requests
+import pandas as pd
+from pandas import *
+import json
+
+url = "https://bg.annapurnapost.com/api/tags/news?page=1&per_page=40&tag=corona-virus"
+
+payload={}
+headers = {}
+res = []
+response = requests.request("GET", url, headers=headers, data=payload)
+data = response.json()
+for i in data['data']:
+    res.append(i)
+
+df = pd.json_normalize(res)
+df.to_csv('result.csv')
+
+data = read_csv("result.csv")
+slug = data['slug'].tolist()
+
+with open('data.json', 'w', encoding='utf-8') as f:
+    json.dump({"titles":slug}, f, ensure_ascii=False, indent=4)
