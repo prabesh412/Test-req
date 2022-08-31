412ce9867c1ea24982f020ee7ca5f3dabd8acb04
prabesh412 <prabeshuprety1@gmail.com>
2022-09-01 00:14:37 +0545

    removed two w

--- main.py
+++ main.py
@@ -7,7 +7,6 @@ def request(page, last):
     result = []
     for r in range(page, last+1):
         url = f"https://bg.annapurnapost.com/api/search?page={page}&title=%E0%A4%95%E0%A5%8B%E0%A4%B0%E0%A5%8B%E0%A4%A8%E0%A4%BE"
-
         payload={}
         headers = {}
         response = requests.request("GET", url, headers=headers, data=payload)
@@ -19,17 +18,8 @@ def request(page, last):
         page += 1
 
     dictobj["title"] =res
-    try:
-        with open('data.json', 'r+', encoding='utf-8') as json_file:
-            listobj = json.load(json_file)
-            listobj["title"] =res
 
-            with open('data.json', 'w', encoding='utf-8') as json_file:
-                json.dump(listobj, json_file, ensure_ascii=False, indent=4, separators=(',', ': '))
-    except:
-        with open('data.json', 'w', encoding='utf-8') as json_file:
-            json.dump(dictobj, json_file, ensure_ascii=False, indent=4, separators=(',', ': '))
-
-
+    with open('data.json', 'w', encoding='utf-8') as json_file:
+        json.dump(dictobj, json_file, ensure_ascii=False, indent=4, separators=(',', ': '))
 
 request(1,10)
