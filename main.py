4eb87f6ac927ed9b7cf734763f437ea360da801d
prabesh412 <prabeshuprety1@gmail.com>
2022-09-01 17:07:22 +0545

    fix

--- main.py
+++ main.py
@@ -2,10 +2,11 @@ import requests
 import json
 
 def request(page, last):
-    res = []
-    dictobj = {}
-    result = []
+
     for r in range(page, last+1):
+        res = []
+        dictobj = {}
+        result = []
         url = f"https://bg.annapurnapost.com/api/search?page={page}&title=%E0%A4%95%E0%A5%8B%E0%A4%B0%E0%A5%8B%E0%A4%A8%E0%A4%BE"
         payload={}
         headers = {}
@@ -16,17 +17,17 @@ def request(page, last):
 
         page += 1
 
-    dictobj["title"] =res
+        dictobj["title"] =res
 
-    try:
-        with open('data.json', 'r+', encoding='utf-8') as json_file:
-            listobj = json.load(json_file)
-            listobj["title"] += res
-            dictobj["title"] = listobj["title"]
-    except:
-        print("error")
+        try:
+            with open('data.json', 'r+', encoding='utf-8') as json_file:
+                listobj = json.load(json_file)
+                listobj["title"] += res
+                dictobj["title"] = listobj["title"]
+        except:
+            print("error")
 
-    with open('data.json', 'w', encoding='utf-8') as json_file:
-        json.dump(dictobj, json_file, ensure_ascii=False, indent=4, separators=(',', ': '))
+        with open('data.json', 'w', encoding='utf-8') as json_file:
+            json.dump(dictobj, json_file, ensure_ascii=False, indent=4, separators=(',', ': '))
 
-request(4,8)
+request(1,20)
